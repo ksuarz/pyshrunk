6994ba25771bee7dcd7fb955d409cd6a3758d2b9
Indraneel Purohit <i.m.indraneel@gmail.com>
2014-09-19 18:48:05 -0400

    stubbed out api calls and added views

--- app.py
+++ app.py
@@ -5,8 +5,27 @@ from flask import Flask, render_template, request, Response, jsonify
 app = Flask(__name__, static_folder='static', static_url_path='')
 
 @app.route('/')
+def root():
+    return render_template('login.html')
+
+@app.route('/login')
+def login():
+    pass
+
+@app.route('/u')
 def home():
+    # fetch links here
     return render_template('main.html')
+
+@app.route('/g')
+def group():
+    # return render_template('group.html')
+    pass
+
+
+""" 
+    need a route for the /shorturl
+"""
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
